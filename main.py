afa57c980bda86e1e99b41b1e52139a71e854850
Andre <dtandre331@gmail.com>
2023-11-04 16:31:51 -0400

    ☃️☃️

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
