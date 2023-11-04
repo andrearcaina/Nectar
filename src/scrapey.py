195f345d7d6cd6f74bfa733de4f40ddadd75782b
Andre <dtandre331@gmail.com>
2023-11-04 17:06:50 -0400

    scrapeyyy 📎📎📎📎📎📎📎📎📎📎📎📎📎

---
+++ src/scrapey.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
+
+HEADERS = ({'User-Agent':
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'})
