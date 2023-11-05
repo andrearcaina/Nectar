b12ebad6898dcdee76b70adb5194c91ba9f2618f
Andre <dtandre331@gmail.com>
2023-11-05 04:11:06 -0500

    frontend (not buying or selling tho) 🐝🤑

---
+++ static/functionality.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#work").click(function() {
+        // Navigate to the about page (about.html)
+        window.location.href = '/about';
+    });
+
+    $("#started").click(function() {
+        // Navigate to the buying page (buying.html)
+        window.location.href = '/buying';
+    });
+});
